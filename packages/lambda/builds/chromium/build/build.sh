5e42a9efecc3c6d01c8a6be8e25762fb27e7f22d
tac-yacht <tac-yacht@users.noreply.github.com>
2021-07-04 14:47:56 +0900

    separate for building and dependencies

--- packages/lambda/builds/chromium/build/build.sh
+++ packages/lambda/builds/chromium/build/build.sh
@@ -16,22 +16,12 @@
 set -e
 
 BUILD_BASE=$(pwd)
+SCRIPT_BASE=$(cd $(dirname $0); pwd)
 VERSION=${VERSION:-master}
 
 printf "LANG=en_US.utf-8\nLC_ALL=en_US.utf-8" >> /etc/environment
 
-# install dependencies
-yum groupinstall -y "Development Tools"
-yum install -y \
-  alsa-lib-devel atk-devel binutils bison bluez-libs-devel brlapi-devel \
-  bzip2 bzip2-devel cairo-devel cmake cups-devel dbus-devel dbus-glib-devel \
-  expat-devel fontconfig-devel freetype-devel gcc-c++ git glib2-devel glibc \
-  gperf gtk3-devel htop httpd java-1.*.0-openjdk-devel libatomic libcap-devel \
-  libffi-devel libgcc libgnome-keyring-devel libjpeg-devel libstdc++ libuuid-devel \
-  libX11-devel libxkbcommon-x11-devel libXScrnSaver-devel libXtst-devel mercurial \
-  mod_ssl ncurses-compat-libs nspr-devel nss-devel pam-devel pango-devel \
-  pciutils-devel php php-cli pkgconfig pulseaudio-libs-devel python python3 \
-  tar zlib zlib-devel
+${SCRIPT_BASE}/dependencies.sh
 
 mkdir -p build/chromium
 
